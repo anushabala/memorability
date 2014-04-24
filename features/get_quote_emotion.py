575a40c14d2d16de26330745851e802af1923108
Anusha Balakrishnan <anusha.bala28@gmail.com>
2014-04-24 19:59:30 -0400

    Modified emotion script t get and save the polarities of all the quotes to eliminate need to access the sentiment analysis API multiple times.

--- features/get_quote_emotion.py
+++ features/get_quote_emotion.py
@@ -1,33 +1,51 @@
 #Written by: Anusha Balakrishnan
 #Date: 4/18/14
 import json
+import pickle
 import urllib
 import urllib2
 
 
-def call_sentiment_analysis_api(sentence):
-    url = "http://text-processing.com/api/sentiment/"
-    param = {"text":sentence}
-    data = urllib.urlencode(param)
+class SentimentAnalyzer:
+    def save_corpus_polarities(self, in_file, path):
+        read_file = file(in_file, 'r')
+        polarity_info = []
+        line = read_file.readline()
+        while line:
+            line = line.strip().split('\t')
+            quote = line[0].strip()
+            mem = line[1].strip()
+            response = self.call_sentiment_analysis_api(quote)
+            json_response = json.loads(response)
+            if json_response==None:
+                break
+            packaged_data = (quote, mem, json_response)
+            print packaged_data
+            polarity_info.append(packaged_data)
+            line = read_file.readline()
 
-    try:
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        page = response.read()
-        return page
-    except urllib2.HTTPError as e:
-        print e.code
-        print e.read()
-        return None
+        pickle.dump(polarity_info, file(path, 'wb'))
+    def call_sentiment_analysis_api(self,sentence):
+        url = "http://text-processing.com/api/sentiment/"
+        param = {"text":sentence}
+        data = urllib.urlencode(param)
 
-'''
-Returns the polarity of a sentence.
-Returns 1 if positive, 0 if neutral, -1 if negative.
-'''
-def get_polarity(response):
-    try:
-        json_response = json.loads(response)
-        label = json_response['label']
+        try:
+            req = urllib2.Request(url, data)
+            response = urllib2.urlopen(req)
+            page = response.read()
+            return page
+        except urllib2.HTTPError as e:
+            print e.code
+            print e.read()
+            return None
+
+    '''
+    Returns the polarity of a sentence.
+    Returns 1 if positive, 0 if neutral, -1 if negative.
+    '''
+    def get_polarity(self,response):
+        label = response['label']
         if label=="neutral" :
             return 0
         elif label=="pos":
@@ -36,23 +54,56 @@ def get_polarity(response):
             return -1
         else:
             return -9
-    except ValueError as v:
-        print response
-    # polarities = json_response['probability']
-    # print polarities
-    # pos = float(polarities['pos'])
-    # neg = float(polarities['neg'])
-    # neutral = float(polarities['neutral'])
-    # max_pol = max(pos, neg, neutral)
-    # if max_pol==pos:
-    #     return 1
-    # elif max_pol==neg:
-    #     return -1
-    # else:
-    #     return 0
 
+        # polarities = json_response['probability']
+        # print polarities
+        # pos = float(polarities['pos'])
+        # neg = float(polarities['neg'])
+        # neutral = float(polarities['neutral'])
+        # max_pol = max(pos, neg, neutral)
+        # if max_pol==pos:
+        #     return 1
+        # elif max_pol==neg:
+        #     return -1
+        # else:
+        #     return 0
+    def get_quote_polarity(self, quote):
+        response = self.call_sentiment_analysis_api(quote)
+        if response==None:
+            return -9
+        try:
+            json_response = json.loads(response)
+            polarity = self.get_polarity(json_response)
+            return polarity
+        except ValueError as v:
+            print v
+            return -9
+
+    def get_emotion_strength(self, quote):
+        response = self.call_sentiment_analysis_api(quote)
+        try:
+            json_response = json.loads(response)
+
+            label = json_response['label']
+            probabilities = json_response['probability']
+            pos = float(probabilities['pos'])
+            neg = float(probabilities['neg'])
+            neutral = float(probabilities['neutral'])
+
+            if label=="neutral":
+                return neutral
+            elif label=="pos":
+                return pos
+            elif label=="neg":
+                return neg
+            else:
+                return -9
+
+        except ValueError as v:
+            print response
     
 def test_corpus_polarity():
+    sentAnalysis = SentimentAnalyzer()
     corpus = file('../quotes.dat', 'r')
     line = corpus.readline()
     pos = {'M':0, 'N':0}
@@ -64,10 +115,7 @@ def test_corpus_polarity():
         line = line.split('\t')
         sentence = line[0]
         mem = line[1].strip()
-        response = call_sentiment_analysis_api(sentence)
-        if response==None:
-            break
-        polarity = get_polarity(response)
+        polarity = sentAnalysis.get_quote_polarity(sentence)
         total[mem]+=1
         if polarity==1:
             pos[mem] +=1
@@ -91,4 +139,21 @@ def test_corpus_polarity():
     print "Percent negative: %f" % (float(neg['N'])/total['N'])
     print "Percent neutral: %f" % (float(neut['N'])/total['N'])
 
-test_corpus_polarity()
+def compare_corpus_polarity(corpus_polarities):
+    corpus = pickle.load(file(corpus_polarities, 'rb'))
+    i=0
+    while i<len(corpus):
+        current_info = corpus[i]
+        next_info= corpus[i+1]
+        print current_info,"\n", next_info
+
+        current_quote = current_info[0]
+        current_mem = current_info[1]
+        current_polarities = current_info[2]
+        i+=2
+
+
+# test_corpus_polarity()
+sentAnalyzer = SentimentAnalyzer()
+sentAnalyzer.save_corpus_polarities('../quotes.dat', 'corpus_polarities.pkl')
+# compare_corpus_polarity('corpus_polarities.pkl')
