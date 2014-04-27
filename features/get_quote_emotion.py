b9d09fe055c8b9d5f9c4ecedf7bb596c927920a5
Anusha Balakrishnan <anusha.bala28@gmail.com>
2014-04-27 03:25:15 -0400

    Added some functions to emotion script to avoid calling API

--- features/get_quote_emotion.py
+++ features/get_quote_emotion.py
@@ -79,29 +79,67 @@ class SentimentAnalyzer:
             print v
             return -9
 
-    def get_emotion_strength(self, quote):
+    def get_emotion_strength(self, response):
+        label = response['label']
+        probabilities = response['probability']
+        pos = float(probabilities['pos'])
+        neg = float(probabilities['neg'])
+        neutral = float(probabilities['neutral'])
+
+        if label=="neutral":
+            return 1-neutral
+        elif label=="pos":
+            return pos
+        elif label=="neg":
+            return neg
+        else:
+            return -9
+
+    def get_quote_strength(self, quote):
         response = self.call_sentiment_analysis_api(quote)
         try:
             json_response = json.loads(response)
-
-            label = json_response['label']
-            probabilities = json_response['probability']
-            pos = float(probabilities['pos'])
-            neg = float(probabilities['neg'])
-            neutral = float(probabilities['neutral'])
-
-            if label=="neutral":
-                return neutral
-            elif label=="pos":
-                return pos
-            elif label=="neg":
-                return neg
-            else:
-                return -9
-
+            strength = self.get_emotion_strength(json_response)
+            return strength
         except ValueError as v:
             print response
-    
+            return -9
+
+
+    def compare_polarities(self, first, second):
+        """
+        Returns 1 if the first quote is emotionally stronger than the second quote
+        Returns -1 if the second quote is emotionally stronger than the first quote
+        :param first: dictionary containing emotion probabilites for the first quote
+        :param second: dictionary containing emotoion probabilities for the second quote
+        """
+        first_label = self.get_polarity(first)
+        second_label = self.get_polarity(second)
+        first_pol = self.get_emotion_strength(first)
+        second_pol = self.get_emotion_strength(second)
+        # If one quote is neutral and the other isn't, the one that isn't is stronger emotionally
+        if first_label==0 and second_label!=0:
+            return -1
+        elif first_label!=0 and second_label==0:
+            return 1
+        # If both quotes are neutral, the one that's less neutral is stronger emotionally
+        elif first_label==0 and second_label==0:
+            if first_pol==second_pol:
+                return 0
+            if min(first_pol, second_pol) == first_pol:
+                return 1
+            else:
+                return -1
+        # Else if both quotes are not neutral, then the one with higher polarity is stronger,
+        # irrespective of whether they are negative or positive
+        else:
+            if first_pol==second_pol:
+                return 0
+            if max(first_pol, second_pol)== first_pol:
+                return 1
+            else:
+                return -1
+
 def test_corpus_polarity():
     sentAnalysis = SentimentAnalyzer()
     corpus = file('../quotes.dat', 'r')
@@ -139,21 +177,35 @@ def test_corpus_polarity():
     print "Percent negative: %f" % (float(neg['N'])/total['N'])
     print "Percent neutral: %f" % (float(neut['N'])/total['N'])
 
-def compare_corpus_polarity(corpus_polarities):
+def compare_quote_strengths(corpus_polarities):
+    sentAnalyzer = SentimentAnalyzer()
     corpus = pickle.load(file(corpus_polarities, 'rb'))
     i=0
+    mem = 0
+    nonmem = 0
+    total = 0
     while i<len(corpus):
-        current_info = corpus[i]
-        next_info= corpus[i+1]
-        print current_info,"\n", next_info
+        mem_info = corpus[i]
+        nonmem_info = corpus[i+1]
 
-        current_quote = current_info[0]
-        current_mem = current_info[1]
-        current_polarities = current_info[2]
+        total += 1
+        mem_polarities = mem_info[2]
+        nonmem_polarities = nonmem_info[2]
+        comparison = sentAnalyzer.compare_polarities(mem_polarities, nonmem_polarities)
+        if comparison==1:
+            mem +=1
+        elif comparison==-1:
+            nonmem+=1
+        elif comparison==0:
+            total-=1
         i+=2
+
+    print "Percentage of pairs where the memorable quote was emotionally stronger: %f" % (float(mem)/total)
+    print "Percentage of pairs where the non-memorable quote was emotionally stronger: %f" % (float(nonmem)/total)
+
 
 
 # test_corpus_polarity()
 sentAnalyzer = SentimentAnalyzer()
-sentAnalyzer.save_corpus_polarities('../quotes.dat', 'corpus_polarities.pkl')
-# compare_corpus_polarity('corpus_polarities.pkl')
+# sentAnalyzer.save_corpus_polarities('../quotes.dat', 'corpus_polarities.pkl')
+compare_quote_strengths('corpus_polarities.pkl')
