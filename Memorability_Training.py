36d15038f47ba51a4850149540f65c56599bf7fa
aarora91 <ashima.arora91@gmail.com>
2014-04-27 11:53:27 -0400

    Updating for syntactic ngrams-Ashima has write token.

--- Memorability_Training.py
+++ Memorability_Training.py
@@ -1,5 +1,6 @@
 import create_training_corpus as ctc
 import pprint
+import nltk
 from nltk.stem.porter import *
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
 stopwords = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your']
@@ -9,7 +10,7 @@ class Train():
         self.memQuotes={}
         self.nonmemQuotes={}
         self.featureList=['Unigram','Bigram']
-        self.unigrams=[]
+        self.unigrams=[]    #Common list for all features.
         self.bigrams=[]
         self.trainfile =  'create_datasets/fv_train'
         self.trainparsed = 'create_datasets/train'#'quotes.dat'
@@ -57,6 +58,13 @@ class Train():
                 filtered_word_list.remove(word) # remove word from filtered_word_list if it is a stopword
         return filtered_word_list
     
+    def removePunctuations(sentence):
+        puncts = '?!,-_.:;\',",'
+        for punct in puncts:
+            if(punct in sentence):
+                sentence = sentence.replace(punct,'')
+        return sentence
+
     def preprocess(self,sentence):
         tokens=self.tokenize(sentence)
         lcased=self.lowercase(tokens)
@@ -158,6 +166,21 @@ class Train():
                 if (i < len(tokens) - 2):
                     trigram = tokens[i]+'_'+tokens[i+1]+'_'+tokens[i+2]
                     self.unigrams.append(trigram)
+
+            #Edits by Ashima Arora: Adding the syntactic ngrams.
+            #Syntax Unigrams
+            for word_tag in nltk.pos_tag(tokens):
+                tag = word_tag[1]
+                self.unigrams.append(tag)
+            #Syntax Bigrams
+            for i in range(0,len(tokens)-1):
+                syntaxBigram = nltk.pos_tag(tokens[i])[1]+"_"+nltk.pos_tag(tokens[i+1])[1]
+                self.unigrams.append(syntaxBigram)
+            #Syntax trigrams
+            for i in range(0,len(tokens)-2):
+                syntaxTrigram = nltk.pos_tag(tokens[i])[1]+"_"+nltk.pos_tag(tokens[i+1])[1]+"_"+nltk.pos_tag(tokens[i+2])[1]
+                self.unigrams.append(syntaxTrigram)
+                
         self.lastIndex = len(self.unigrams)+1
 
 
@@ -166,14 +189,13 @@ class Train():
     #Reading the training file and creating the feature vector for that
     #train.readfile(train.trainfile,train.trainparsed)
 
-for fold in range(1,6):
+for fold in range(1,1):
     train=Train()
     train_file = train.trainparsed+str(fold)+".dat"
     train.buildQuoteDictionaries(train_file)
     train.buildFeatureDictionaries()
-    train.buildFeatureFile(train_file, train.trainfile, fold)#(train.trainfile)
+    # train.buildFeatureFile(train_file, train.trainfile, fold)#(train.trainfile)
 
-    test_file = train.testparsed+str(fold)+".dat"
-    train.buildQuoteDictionaries(test_file)
-    train.buildFeatureFile(test_file, train.testfile, fold )
-
+    # test_file = train.testparsed+str(fold)+".dat"
+    # train.buildQuoteDictionaries(test_file)
+    # train.buildFeatureFile(test_file, train.testfile, fold )
